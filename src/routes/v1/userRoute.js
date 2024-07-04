4daea77ceb8afaa5c98ec83bfef9701dc996a58c
NguyenDat-2003 <maxdat2003@gmail.com>
2024-07-04 14:18:24 +0700

    authorization_authentication

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { authController } from '~/controllers/authController'
 import { userController } from '~/controllers/userController'
+import { restrictTo } from '~/middlewares/restrictTo'
+import { verifyToken } from '~/middlewares/verifyToken'
 import { userValidation } from '~/validations/userValidation'
 
 const router = express.Router()
@@ -9,6 +11,10 @@ router.post('/signup', userValidation.createNewUser, authController.signup)
 router.post('/login', authController.login)
 router.post('/logout', authController.logout)
 
+//---Protect all routes after this middleware
+router.use(verifyToken)
+
+router.use(restrictTo('admin'))
 router.route('/').get(userController.getAllUser).post(userValidation.createNewUser, userController.createNewUser)
 router.route('/:id').get(userController.getUser).put(userValidation.updateUser, userController.updateUser).delete(userController.deleteUser)
 
