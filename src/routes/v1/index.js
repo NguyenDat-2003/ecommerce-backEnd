9c21c15d89b739722bc20d5a7a6d1d46bdd1ebe5
NguyenDat-2003 <maxdat2003@gmail.com>
2024-07-05 14:56:54 +0700

    CRUD_Blog_handling_route_like_dislike_blog

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { userRoute } from './userRoute'
 import { productRoute } from './productRoute'
+import { blogRoute } from './blogRoute'
 
 const router = express.Router()
 
 router.use('/users', userRoute)
 router.use('/products', productRoute)
+router.use('/blogs', blogRoute)
 
 export const APIs_V1 = router
